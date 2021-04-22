8a9620470e7fd04f07c05b650cfa408d3ed789fe
Dilsonjoan16 <dilsonjoan16@gmail.com>
2021-04-22 14:47:31 -0400

    primeros cambios

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+//importamos Boostrap
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 Vue.config.productionTip = false
 
 new Vue({
